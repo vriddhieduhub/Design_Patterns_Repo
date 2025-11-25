5f9559c8df513e3eaf93369be81de700c262abd4
Deepjyoty Roy <vriddhieduhub@gmail.com>
2025-11-26 01:56:16 +0530

    Code Folder Structure create || Initital commit

---
+++ Creational_Design_Pattern/Factory_DesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factory_DesignPattern
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Factory_DesignPattern!");
+        }
+    }
+}
---
+++ Creational_Design_Pattern/Singleton_DesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Singleton_DesignPattern
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Singleton_DesignPattern!");
+        }
+    }
+}
